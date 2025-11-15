aa0e950c0a2564193b65f95205b3cf1a914a30fa
iizom <1739645729@qq.com>
2025-11-15 16:39:10 +0800

    migrate blogmodels

--- blogMain/models.py
+++ blogMain/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100,unique=True,null=False)    
+    class Meta:
+        db_table = 'blog_category'
+    def __str__(self):
+        return self.name
+    
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    publicTime = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(BlogCategory, on_delete=models.SET_DEFAULT,default=1)
+    auther = models.ForeignKey('auth.User', on_delete=models.SET_DEFAULT,default=1)
+    class Meta:
+        db_table = 'blog_post'
+    def __str__(self):
+        return self.title
+    
+class BlogComment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE,related_name='comments')
+    auther = models.ForeignKey('auth.User', on_delete=models.SET_DEFAULT,default=1)
+    content = models.TextField()
+    publicTime = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table = 'blog_comment'
+    def __str__(self):
+        return self.content[:20]
